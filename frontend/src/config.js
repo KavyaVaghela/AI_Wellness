9966c541944cb4a9223bbd7633fd83c9249f2c0c
Vaghela Kavya <kavyakumarvaghela@gmail.com>
2026-02-03 11:55:35 +0530

    kk

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.PROD
-    ? 'https://ai-wellness-companion-1-7wie.onrender.com'
+    ? 'https://ai-wellness-q1n8.onrender.com'
     : 'http://localhost:5000';
 
 console.log("API URL Used:", API_URL);
